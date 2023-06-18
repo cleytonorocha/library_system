b444a2de358d164d688a8bd4f901be4debe49eda
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-06-18 15:41:50 -0300

    Base project

---
+++ src/main/java/com/github/cleyto_orocha/library_system/LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.cleyto_orocha.library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cleyto_orocha/library_system/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cleyto_orocha.library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
