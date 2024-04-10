1f7f28a06dff09b936ca237ac22f73d6b1cb44b2
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-10 09:35:08 -0300

    fix: bug identity

--- src/main/java/com/github/cleyto_orocha/library_system/security/JwtAuthFilter.java
+++ src/main/java/com/github/cleyto_orocha/library_system/security/JwtAuthFilter.java
@@ -2,6 +2,7 @@ package com.github.cleyto_orocha.library_system.security;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,7 +26,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final UsersRepository usersRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain)
             throws ServletException, IOException {
         String token = this.recoveryToken(request);
         if (token != null) {
