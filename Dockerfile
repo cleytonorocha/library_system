d0858f8ac9b33c2e157d81c0f0cadbc9fd1657a0
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-07 21:16:07 -0300

    fix: Cors https #1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:17-jdk-alpine
+FROM eclipse-temurin:17
 WORKDIR /app
 COPY target/library_system-1.0.0.jar library_system.jar
 EXPOSE 8080
--- src/main/java/com/github/cleyto_orocha/library_system/LibrarySystemApplication.java
+++ src/main/java/com/github/cleyto_orocha/library_system/LibrarySystemApplication.java
@@ -3,7 +3,9 @@ package com.github.cleyto_orocha.library_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @EnableJpaRepositories
 @SpringBootApplication
 public class LibrarySystemApplication {
