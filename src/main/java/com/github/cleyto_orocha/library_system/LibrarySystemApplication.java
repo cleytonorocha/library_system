ad9294a3a96e82af069a329e5c81351d8773dfbc
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-09-12 19:08:02 -0300

    fix: set the language localeresolver

--- src/main/java/com/github/cleyto_orocha/library_system/LibrarySystemApplication.java
+++ src/main/java/com/github/cleyto_orocha/library_system/LibrarySystemApplication.java
@@ -1,9 +1,14 @@
 package com.github.cleyto_orocha.library_system;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @CrossOrigin(origins = "*")
 @EnableJpaRepositories
@@ -12,6 +17,12 @@ public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		// Definir o Locale fixo para inglês
+		return new FixedLocaleResolver(Locale.ENGLISH);
 	}
 
 }
